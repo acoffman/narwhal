58f0a3f6a644090039db62234acd70406c77f40d
Delia Feerer <delia@delia-feerers-macbook.local>
2009-12-12 12:58:10 -0600

    page switching

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,4 +2,21 @@ class DashboardController < ApplicationController
 def index
 	render :overview
 end
+
+def overview
+  render :overview
+end
+
+def ip
+  render :ip
+end
+
+def protocols
+  render :protocols
+end
+
+def rate
+  render :rate
+end
+
 end 
