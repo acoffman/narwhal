67ea9fd39e663e22f7f2f96c8387e325585d1f75
Adam Coffman <coffman.adam@gmail.com>
2009-12-12 16:25:36 -0600

    refactored chart code

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -19,11 +19,7 @@ def overview
   dataset4 = GoogleChartDataset.new :data => [@test_num, @test_num], :color => 'FF0000', :title => 'Allowable Rate'
   dataset1 = GoogleChartDataset.new :data => [10, 20, 30, 40, 50]
   dataset2 = GoogleChartDataset.new :data => [10,50,4,10,16], :color => 'FF0000', :title => 'Traffic'
-  data = GoogleChartData.new :datasets => [dataset1, dataset2, dataset3, dataset4]
-  axis = GoogleChartAxis.new :axis  => [GoogleChartAxis::LEFT, GoogleChartAxis::BOTTOM]
-  @chart = GoogleLineChart.new :chart_type => GoogleLineChart::XY , :width => 500, :height => 300, :title => ['Incoming Traffic Rate', 'Configured Allowable Peak and Average Rates']
-  @chart.data = data
-  @chart.axis = axis
+  @chart = init_chart(["Incoming Traffic", "Average Rates"], [dataset1, dataset2, dataset3, dataset4])
 end
 
 def ip
@@ -41,4 +37,13 @@ def rate
   render :rate
 end
 
+private
+  def init_chart(title, datasets)
+    data = GoogleChartData.new :datasets => datasets
+    axis = GoogleChartAxis.new :axis  => [GoogleChartAxis::LEFT, GoogleChartAxis::BOTTOM]
+    chart = GoogleLineChart.new :chart_type => GoogleLineChart::XY , :width => 500, :height => 300, :title => title
+    chart.data = data
+    chart.axis = axis
+    chart
+  end
 end
