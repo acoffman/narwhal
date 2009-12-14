3262f6101b296476a1da8a96f4722e24180bba57
Adam Coffman <coffman.adam@gmail.com>
2009-12-14 00:26:06 -0600

    updated chart

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -17,11 +17,13 @@ def overview
   @test_num = params[:test_num]
   @test_num ||= 55
   @nav_over = "current"
+  dataset5 = GoogleChartDataset.new :data => [0,100]
+  dataset6 = GoogleChartDataset.new :data => [@test_num, @test_num], :color => '0000FF', :title => 'Allowable Average Rate'
   dataset3 = GoogleChartDataset.new :data => [0,100]
-  dataset4 = GoogleChartDataset.new :data => [@test_num, @test_num], :color => 'FF0000', :title => 'Allowable Rate'
-  dataset1 = GoogleChartDataset.new :data => [10, 20, 30, 40, 50]
-  dataset2 = GoogleChartDataset.new :data => [10,50,4,10,16], :color => 'FF0000', :title => 'Traffic'
-  @chart = init_chart(["Incoming Traffic", "Average Rates"], [dataset1, dataset2, dataset3, dataset4])
+  dataset4 = GoogleChartDataset.new :data => [75, 75], :color => 'FF0000', :title => 'Allowable Peak Rate'
+  dataset1 = GoogleChartDataset.new :data => [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+  dataset2 = GoogleChartDataset.new :data => [10, 50, 24, 10, 26, 32, 45, 43, 23, 42, 65], :color => '000000', :title => 'Traffic'
+  @chart = init_chart("Incoming Traffic Average Rates", [dataset1, dataset2, dataset3, dataset4, dataset5, dataset6])
 end
 
 def ip
