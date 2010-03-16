dbfdff1182a2a9852837927f7f618d4c35608985
Adam Coffman <coffman.adam@gmail.com>
2010-03-16 18:00:23 -0500

    dont always run test code

--- audit daemon/audit_daemon_client.rb
+++ audit daemon/audit_daemon_client.rb
@@ -1,16 +1,16 @@
 require 'rubygems'
 require 'thrift'
 require File.dirname(__FILE__) + '/generated/gen-rb/audit_daemon'
- 
+
 include AuditDaemon
 
 class DaemonClient
   def initialize
-      @transport = Thrift::BufferedTransport.new(Thrift::Socket.new('127.0.0.1', 9090))
-      protocol = Thrift::BinaryProtocol.new(@transport)
-      @client = Client.new(protocol)
-      
-      @transport.open()
+    @transport = Thrift::BufferedTransport.new(Thrift::Socket.new('127.0.0.1', 9090))
+    protocol = Thrift::BinaryProtocol.new(@transport)
+    @client = Client.new(protocol)
+
+    @transport.open()
   end
 
   def send(message)
@@ -26,10 +26,12 @@ class DaemonClient
   end
 end
 
-msg = Message.new
-msg.msg = "Hi brent"
+if $0 == __FILE__
+  msg = Message.new
+  msg.msg = "y helo thar"
 
-client = DaemonClient.new
-client.send(msg)
+  client = DaemonClient.new
+  client.send(msg)
+end
 
 
