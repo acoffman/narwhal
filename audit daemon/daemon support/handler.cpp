68aeb59a153438b8e11153db40e640492f3f345b
Adam Coffman <coffman.adam@gmail.com>
2010-03-17 18:48:13 -0500

    handler starting to come together, added makefile

---
+++ audit daemon/daemon support/handler.cpp
@@ -0,0 +1,40 @@
+NotificationHandler::NotificationHandler(int userid){
+  this->userid = userid;
+  driver = get_driver_instance();
+  con = driver->connect(HOST, USER, PASS);
+  stmnt = con->createStatement();
+  stmnt->execute("use "  TABLE);
+  performQuery();
+  generateKeys();
+  createFilter();
+  mapBits();
+};
+
+NotificationHandler::~NotificationHandler(){
+  delete con;
+  delete stmnt;
+  delete res;
+};
+
+void NotificationHandler::createFilter(){
+  filter = new BloomFilter(calculateFilterSize());
+  for(int i =0; i < keyList.size(); i++){
+    (*filter).add(keyList.at(i));
+  }
+};
+
+void NotificationHandler::performQuery(){
+  //do db query here
+};
+
+void NotificationHandler::generateKeys(){
+  //populate vector with keys from the query
+};
+
+int NotificationHandler::calculateFilterSize(){
+  return (int) abs(ceil((keyList.size() * log(FP_PERCENT))/(log(2) * log(2))));
+};
+
+void NotificationHandler::mapBits(){
+  //map bits into kernel space
+};
--- audit daemon/daemon support/handler.h
+++ audit daemon/daemon support/handler.h
@@ -1,21 +1,46 @@
 #ifndef HANDLER_H
 #define HANDLER_H
 
+#include "../bloom/bloomfilter.h"
+
+#include "mysql_connection.h"
+
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+
+#include <vector>
+#include <string>
+#include <cmath>
 
 using namespace std;
 
+#define HOST "localhost"
+#define USER "root"
+#define PASS ""
+#define TABLE "tablename"
+
+#define FP_PERCENT 0.95
+
 class NotificationHandler {
   public:
-    BloomFilter(int init_size);
-    ~BloomFilter();
-    void add(string item);
-    bool include(string item);
+    NotificationHandler(int userid);
+    ~NotificationHandler();
 
   private:
-    boost::dynamic_bitset<> * bitset;
-    int size;
-    int djbHash(string item);
-    int* keys(string item);
+    int userid;
+    BloomFilter * filter;
+    vector<string> keyList;
+    sql::Driver * driver;
+    sql::Connection * con;
+    sql::Statement * stmnt;
+    sql::ResultSet * res;
+    void performQuery();
+    int calculateFilterSize();
+    void createFilter();
+    void mapBits();
+    void generateKeys();
 
 };
 #include "handler.cpp"
