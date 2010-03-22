27eac5ab4ca5fc99e30a008e2a55de605ad1f41f
Mark Sands <marksands07@gmail.com>
2010-03-22 04:40:16 -0500

    added protocol model

--- interface/db/migrate/20100211021825_initial_migration.rb
+++ interface/db/migrate/20100211021825_initial_migration.rb
@@ -4,22 +4,23 @@ class InitialMigration < ActiveRecord::Migration
     create_table :users do |t|
       t.string :username
       t.string :password
-      t.string :ip
     end
 
     create_table :blockeds do |t|
       t.integer :userid
       t.string :ip
+    end
+
+    create_table :protocols do |t|
       t.string :port
       t.string :protocol
     end
-
   end
 
   def self.down
     drop_table :blockeds
     drop_table :users
-
+    drop_table :protocols
   end
 end
 
