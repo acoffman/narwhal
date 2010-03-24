56ce52de50753bd6a7fc7b042ef4bc5447c49bdd
Adam Coffman <coffman.adam@gmail.com>
2010-03-23 22:55:44 -0500

    debug

--- audit daemon/daemon support/handler.cpp
+++ audit daemon/daemon support/handler.cpp
@@ -33,8 +33,8 @@ void NotificationHandler::performQuery(){
 void NotificationHandler::generateKeys(){
   cout << "IPS:" << endl;
   while (res->next()) {
-    cout << res->getString("ip") << endl;
     keyList.push_back(res->getString("ip"));
+    cout << res->getString("ip") << endl;
   }
 };
 
