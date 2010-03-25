f7a93a067885a31ca51ef79d24e30646fe45c343
Brent Beer <brentbeer@client46-9.wifi.siue.edu>
2010-03-25 03:15:37 -0500

    jquery working success

--- interface/public/javascripts/application.js
+++ interface/public/javascripts/application.js
@@ -1,6 +1,8 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-
 jQuery(function() {
-    $('input[type="text"]:first').focus();
+  //  $("form input:text:first").css({background:"yellow", border:"3px red solid"});
+$("input[type='text']:first").focus();
+
+	
 });
