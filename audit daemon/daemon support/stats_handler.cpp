77fd46c20a06f4cffe9952a0540659464a6254d0
Adam Coffman <coffman.adam@gmail.com>
2010-03-31 01:43:58 -0500

    no. just no.

--- audit daemon/daemon support/stats_handler.cpp
+++ audit daemon/daemon support/stats_handler.cpp
@@ -18,7 +18,7 @@ void StatsNotificationHandler::getKernelStats(){
   //ioctl(file_desc,STAT_IOCTL,&currentStats);
   ioctl(file_desc,STAT_IOCTL,myint);
   close(file_desc);
-  cout << myint << endl;
+  cout << *myint << endl;
   delete myint;
 };
 
