cf3fbaed9f55ee9ba9195db5349be869ab8ce723
andrew <drewhaus@gmail.com>
2010-03-30 23:27:17 -0500

    l

--- driver/ti/if_ti.c
+++ driver/ti/if_ti.c
@@ -3856,7 +3856,7 @@ ti_ioctl2(struct cdev *dev, u_long cmd, caddr_t addr, int flag,
 
 			device_printf(sc->ti_dev,"got a stat cmd!, dropped packets %lu, received %lu, total %lu\n",stats->dropped_pkts,stats->num_pkts,stats->data);
 
-			if(copyout(&(caddr_t)stats,addr,sizeof(struct stat_ctl *)) == EFAULT)
+			if(copyout((caddr_t)&stats,addr,sizeof(struct stat_ctl *)) == EFAULT)
 			{
 					device_printf(sc->ti_dev,"bad copy out\n");
 					return EINVAL;
