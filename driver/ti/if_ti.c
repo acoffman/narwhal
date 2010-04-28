38068cb92e90e1ce18db717d9b64f8ab0c5913df
andrew <drewhaus@gmail.com>
2010-04-28 18:12:58 -0500

    reset

--- driver/ti/if_ti.c
+++ driver/ti/if_ti.c
@@ -3865,8 +3865,10 @@ ti_ioctl2(struct cdev *dev, u_long cmd, caddr_t addr, int flag,
 			/*if(stats == NULL)*/
 				/*return EINVAL;*/
 
-			device_printf(sc->ti_dev,"got a stat cmd!, dropped packets %lu, received %lu, total %lu\n",stats.dropped_pkts,stats.num_pkts,stats.data);
-			/*device_printf(sc->ti_dev,"got a stat cmd!, dropped packets %lu, received %lu, total %lu\n",stats->dropped_pkts,stats->num_pkts,stats->data);*/
+			device_printf(sc->ti_dev,"got a stat cmd!, dropped packets %lu, received %lu, total %lu\n",
+										stats.dropped_pkts,stats.num_pkts,stats.data);
+			/*device_printf(sc->ti_dev,"got a stat cmd!, dropped packets %lu, received %lu, total %lu\n",
+			 * 						stats->dropped_pkts,stats->num_pkts,stats->data);*/
 
 			korea = 5;
 
