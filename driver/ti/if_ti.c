3a4cae76fcb7df08eeb455a49f2186af99810be5
andrew <drewhaus@gmail.com>
2010-04-28 19:26:18 -0500

    l

--- driver/ti/if_ti.c
+++ driver/ti/if_ti.c
@@ -3874,7 +3874,7 @@ ti_ioctl2(struct cdev *dev, u_long cmd, caddr_t addr, int flag,
 			hope = &h;
 
 			/*if(copyout(&stats, (caddr_t)addr ,sizeof(struct stat_ctl)))*/
-			if(copyout(&h, addr, sizeof(int)))
+			if(copyout(hope, addr, sizeof(int)))
 			{	
 					device_printf(sc->ti_dev,"bad copy out\n");
 					return EFAULT;
