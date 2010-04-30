6714394b58731acca9a6f33a7b6f6487628e2867
andrew <drewhaus@gmail.com>
2010-04-30 01:24:26 -0500

    .

--- driver/ti/if_ti.c
+++ driver/ti/if_ti.c
@@ -3876,14 +3876,16 @@ ti_ioctl2(struct cdev *dev, u_long cmd, caddr_t addr, int flag,
 			 * 						stats->dropped_pkts,stats->num_pkts,stats->data);*/
 			device_printf(sc->ti_dev,"got a stat cmd!\n");
 
+			struct stats_ctl * stats = (struct stats_ctl *)addr;
 			/*int h = 5/;*/
+
 			long data[3];
 			data[0] = 1;
 			data[1] = 1;
 			data[2] = 1;
 
 			/*if(copyout(&stats, (caddr_t)addr ,sizeof(struct stat_ctl)))*/
-			if(copyout(data, ((struct stats_ctl*) addr)->p, ((struct stats_ctl*) addr)->s) == EFAULT)
+			if(copyout(data, stats->p, stats->s) == EFAULT)
 			/*if(copyout(&h,addr,sizeof(int)) == EFAULT)*/
 			{	
 					device_printf(sc->ti_dev,"bad copy out, address\n");
