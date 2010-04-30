d3473ee9ac36f39bb2858c1419b54ab4dd8a3298
andrew <drewhaus@gmail.com>
2010-04-30 01:33:49 -0500

    .

--- audit daemon/daemon support/stats_handler.cpp
+++ audit daemon/daemon support/stats_handler.cpp
@@ -24,7 +24,7 @@ void StatsNotificationHandler::getKernelStats(){
   ioctl(file_desc,STAT_IOCTL,&stats);
   close(file_desc);
 	long * temp = (long *)stats.p;
-	cout << *temp << endl;
+	cout << (temp + 1) << endl;
 	cout << "finished copy" << endl;
 };
 
