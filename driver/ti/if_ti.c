2e56f9c5db2710b24c46e45d52164186e5704196
andrew <drewhaus@gmail.com>
2010-05-04 23:16:40 -0500

    .

--- driver/ti/if_ti.c
+++ driver/ti/if_ti.c
@@ -3882,7 +3882,7 @@ ti_ioctl2(struct cdev *dev, u_long cmd, caddr_t addr, int flag,
 			long data[3] = {1,1,1};
 
 			device_printf(sc->ti_dev,"stats->p: %d   stats->s: %d   stats: %d\n",
-																(int)stats->p,stats->s,stats);
+																(int)stats->p,stats->s,(int)stats);
 
 			if(copyout(&data, stats->p, sizeof(data)) == EFAULT)
 			{	
